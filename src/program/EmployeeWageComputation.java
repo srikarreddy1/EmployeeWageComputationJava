9839859a4854dada6bc5f4e021d56aa9dd6b1bb5
Srikarreddy1 <fddxzf@gmail.com>
2020-10-16 22:04:02 +0530

    welcome message on the master branch

---
+++ src/program/EmployeeWageComputation.java
@@ -0,0 +1,8 @@
+package program;
+
+public class EmployeeWageComputation {
+    public static void main(String[] args) {
+        System.out.println("welcome to employee wage computation main branch");
+    }
+    
+}
