b09c0528856791392303309f0cbb4d284a75e4c2
Srikarreddy1 <fddxzf@gmail.com>
2020-10-16 22:28:20 +0530

    Attendence use case added

--- src/program/EmployeeWageComputation.java
+++ src/program/EmployeeWageComputation.java
@@ -1,8 +1,16 @@
 package program;
 
+import utilities.Utilities;
+/*author:srikar reddy
+  date:16/10/2020
+  Functionality:To solve the employee wage
+  computation problem
+* */
 public class EmployeeWageComputation {
     public static void main(String[] args) {
         System.out.println("welcome to employee wage computation main branch");
+        Utilities utilities=new Utilities();
+        utilities.Attendence();
     }
     
 }
